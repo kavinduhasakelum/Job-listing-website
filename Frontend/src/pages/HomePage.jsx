359d5fbcf8cbc40737ebd63a967282bdb9d740e5
SuneraR <114847925+SuneraR@users.noreply.github.com>
2025-08-08 20:52:26 +0530

    Added Tailwind to project

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
+import React from 'react';
+
 function HomePage() {
-    return ( <h1>This is Home page!</h1> );
+    return ( <h1 className='text-3xl font-semibold text-amber-300'>This is Home page!</h1> );
 }
 
 export default HomePage;
