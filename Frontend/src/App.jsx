06758f118b41645aa4eda6ef5c2124bade2704b8
SuneraR <114847925+SuneraR@users.noreply.github.com>
2025-08-08 20:36:17 +0530

    Added Tailwind to project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React,{ useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1 className='text-3xl font-semibold text-amber-300'>Hello</h1>
     </>
   )
 }
