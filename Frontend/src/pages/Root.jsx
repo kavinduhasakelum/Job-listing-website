cff59e83605cc5b4ce7883beaddc7e92e45a242f
dihan.shanuka <udshanuka11@gmail.com>
2025-10-01 02:33:29 +0530

    Added professional footer with WorkNest branding and icons

--- Frontend/src/pages/Root.jsx
+++ Frontend/src/pages/Root.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 function Root() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar */}
       <NavBar />
-      <main className="mx-auto flex flex-col gap-6 max-w-screen-xl px-4 sm:px-6 lg:px-8">
+
+      {/* Page content */}
+      <main className="flex-grow mx-auto flex flex-col gap-6 max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <Outlet />
       </main>
-    </>
+
+      {/* Footer */}
+      <Footer />
+    </div>
   );
 }
 
